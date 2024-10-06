ba036bd788600ccd370c9f2d6d69d399be28db9d
Harbaoui Ghaith <89579957+HARBA0Ui@users.noreply.github.com>
2024-10-06 15:06:49 +0100

    Logo update

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ const Logo = ({bg= "white"}) => {
   return (
     <Link to="/" reloadDocument className="flex items-center gap-x-2 cursor-pointer !bg-transparent">
         <img src="/logo.png" className={`${bg == "black" && "invert"} w-12 md:w-16 h-12 md:h-16 `}/>
-        <h1 className={`${bg == "black" && "text-white"} text-2xl md:text-3xl`}>Fashions Finds</h1>
+        <h1 className={`${bg == "black" && "text-white"} text-2xl md:text-3xl`}>FashionFinds</h1>
     </Link>
   )
 }
